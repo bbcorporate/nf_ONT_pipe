3f00139bb8da41a58de561a48df2c26e4b4b6237
Simon P <prochnik@gmail.com>
2023-12-28 15:22:10 -0800

    working on automated pipeline

--- run_job_as_batches.sh
+++ run_job_as_batches.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+this_release=${1:-v0.1}
+cont_pieces="'"`printf '{"command": ["bbcorporate/nf_ONT_pipe", "-r %s", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1", "--depth 10"]}' $this_release`"'"
 
-    #cmd=`printf '{"command": ["bbcorporate/nf_ONT_pipe", "-r d973094", "--ref DSB127_701_NoStop_UpdatedHisReference.fa", "--gff3 DSB127_701_NoStop_UpdatedHisReference.gff3", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile %s", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1"]}' $i`
-    #echo $cmd
+echo $cont_pieces
+#exit 0
+
     aws --profile birch batch submit-job \
     --job-name nfONTpipe_job_testing \
     --job-queue priority-gwfcore \
     --job-definition nextflow-nextflow-resources \
-    --container-overrides '{"command": ["bbcorporate/nf_ONT_pipe", "-r d973094", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1"]}
+    --container-overrides $cont_pieces
 
     #echo Submitted $i
     
