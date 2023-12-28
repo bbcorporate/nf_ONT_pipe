fb3df667848bff1d97529badb9dbc6a0b262c457
Simon P <prochnik@gmail.com>
2023-12-28 15:33:45 -0800

    update

--- run_job_as_batches.sh
+++ run_job_as_batches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 this_release=${1:-v0.1}
-cont_pieces="'"`printf '{"command": ["bbcorporate/nf_ONT_pipe", "-r %s", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1", "--depth 10"]}' $this_release`"'"
+cont_pieces=`printf '{"command": ["bbcorporate/nf_ONT_pipe", "-r %s", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1", "--depth 10"]}' $this_release`
 
 echo $cont_pieces
 #exit 0
@@ -9,7 +9,7 @@ echo $cont_pieces
     --job-name nfONTpipe_job_testing \
     --job-queue priority-gwfcore \
     --job-definition nextflow-nextflow-resources \
-    --container-overrides $cont_pieces
+    --container-overrides="'"$cont_pieces"'"
 
     #echo Submitted $i
     
