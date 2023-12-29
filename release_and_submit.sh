e9c4e9bb9e704bc01b400e307b43d84630eeeefa
Simon P <prochnik@gmail.com>
2023-12-29 13:13:07 -0800

    update

--- release_and_submit.sh
+++ release_and_submit.sh
@@ -5,5 +5,5 @@ git push origin main
 next_vers=`gh release list | grep Latest | perl -ne '($a) = /(v\d+\.\d+\.\d+)/; $a =~ s/(\d+)$/1+$1/e; print "$a\n" '`
 echo Making next release: $next_vers
 gh release create $next_vers  --notes "${1:-release}"
-./run_job_as_batches.sh $next_vers
+./run_job_as_batches.pl $next_vers
 
--- run_job_as_batches.sh
+++ run_job_as_batches.sh
@@ -3,13 +3,14 @@ this_release=${1:-v0.1}
 cont_pieces=`printf '{"command": ["bbcorporate/nf_ONT_pipe", "-r %s", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1", "--depth 10"]}' $this_release`
 
 echo $cont_pieces
+
 #exit 0
 
     aws --profile birch batch submit-job \
     --job-name nfONTpipe_job_testing \
     --job-queue priority-gwfcore \
     --job-definition nextflow-nextflow-resources \
-    --container-overrides="'"$cont_pieces"'"
+    --container-overrides 5'{"command": ["bbcorporate/nf_ONT_pipe", "-r Latest", "--ref INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.fa", "--gff3 INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gff3", "--gb INS00182_DSB127_701_NoStop_UpdatedHisMapEngTag.gb", "--s3dir s3://bb-aws-genomics-workflows-store/nf-pipeline-testing/nf_ONT_pipe/snip.310memberLibrary", "--fqfile snip.250k.reads.fq", "--intervals DSB127_701_NoStop_UpdatedHisMap:4218-5090", "--ploidy 1", "--expreadlen 960", "--slop 1", "--depth 10"]}'
 
     #echo Submitted $i
     
