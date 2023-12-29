163f29aca5aef3265f68cf0d803de98a41d6a605
Simon P <prochnik@gmail.com>
2023-12-29 13:13:32 -0800

    .

--- release_and_submit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-git commit -m "${1:-release}" main.nf
-git push origin main
-# add one to last number in current version
-next_vers=`gh release list | grep Latest | perl -ne '($a) = /(v\d+\.\d+\.\d+)/; $a =~ s/(\d+)$/1+$1/e; print "$a\n" '`
-echo Making next release: $next_vers
-gh release create $next_vers  --notes "${1:-release}"
-./run_job_as_batches.pl $next_vers
-
